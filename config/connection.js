2d4f03cb0672b6d5309c2f37e1b97a6808dd8fd1
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-16 18:56:27 -0700

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,14 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    : new Sequelize('ecommerce_db','root', 'root', {
+      host: '127.0.0.1',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
-        decimalNumbers: true,
-      },
+        socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+    },
     });
 
 module.exports = sequelize;
